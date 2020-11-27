fb3e6dec0e2494597297d0433105553a6e6cadf3
eForce-Tsai <39614634+eForce-Tsai@users.noreply.github.com>
2020-11-27 18:58:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 let ring = neopixel.create(DigitalPin.P16, 8, NeoPixelMode.RGB)
 basic.forever(function () {
     for (let index = 0; index <= 23; index++) {
-        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
+        ring.setPixelColor(index, randint(0, 255))
         ring.show()
-        basic.pause(50)
+        basic.pause(10)
         ring.setPixelColor(index, neopixel.rgb(16, 16, 16))
         ring.show()
     }
